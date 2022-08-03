e99914e894efcbd6619096c1c85f529fd30da133
“nurimammasri” <54845293+nurimammasri@users.noreply.github.com>
2022-08-04 02:56:06 +0800

    Update 1_🤓_Homepage.py

--- Dashboard/1_🤓_Homepage.py
+++ Dashboard/1_🤓_Homepage.py
@@ -81,7 +81,7 @@ with row1_1:
 
 # Clean - AIR QUALITY INDEX (by cities).csv
 # df_aqicty = pd.read_csv("https://drive.google.com/uc?id=1V086i1eHdM08nk67F4l2D7_bj-ZJk8PY")
-df_aqicty = pd.read_csv("../data/Most Polluted Cities and Countries (IQAir Index)/Clean - AIR QUALITY INDEX (by cities).csv")
+df_aqicty = pd.read_csv("/../data/Most Polluted Cities and Countries (IQAir Index)/Clean - AIR QUALITY INDEX (by cities).csv")
 
 # Clean - AIR QUALITY INDEX- top countries.csv
 # df_aqitpcr = pd.read_csv("http s://drive.google.com/uc?id=11qjUGvAQiqEgfPWW8USMz6rHlARcrL_P")
