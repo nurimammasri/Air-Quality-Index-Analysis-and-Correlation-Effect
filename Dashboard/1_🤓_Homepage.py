9c59f6bbf56392d9529514f4cf55adffa6ef3677
“nurimammasri” <54845293+nurimammasri@users.noreply.github.com>
2022-09-05 23:51:55 +0800

    Update 1_🤓_Homepage.py

--- Dashboard/1_🤓_Homepage.py
+++ Dashboard/1_🤓_Homepage.py
@@ -116,12 +116,12 @@ with row1_1:
 
 
 with row1_2:
-    if os.path.dirname(os.getcwd()) == "/app":
-        d = "/app/air-quality-index-analysis-and-correlation-effect/"
-    else:
-        d=""
-    image = Image.open(d+'images/air-pollution-smoke-emission.jpg')
-    st.image(image) # caption='air-pollution-smoke-emission.jpg'
+    # if os.path.dirname(os.getcwd()) == "/app":
+    #     d = "/app/air-quality-index-analysis-and-correlation-effect/"
+    # else:
+    #     d=""
+    # image = Image.open(d+'images/air-pollution-smoke-emission.jpg')
+    # st.image(image) # caption='air-pollution-smoke-emission.jpg'
 
 ################
 ### ANALYSIS ###
