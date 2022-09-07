11dc81c6705f85a20ea7152de74fc2a2f16799d5
“nurimammasri” <54845293+nurimammasri@users.noreply.github.com>
2022-09-08 01:49:41 +0800

    Update 2_📚_Projects.py

--- Dashboard/pages/2_📚_Projects.py
+++ Dashboard/pages/2_📚_Projects.py
@@ -9,24 +9,24 @@ st.set_page_config(
 
 st.title("Projects")
 
-def show_pdf(file_path):
-    with open(file_path,"rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-    # st.text(pdf_display)
-    st.markdown(pdf_display, unsafe_allow_html=True)
+# def show_pdf(file_path):
+#     with open(file_path,"rb") as f:
+#         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+#     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+#     # st.text(pdf_display)
+#     st.markdown(pdf_display, unsafe_allow_html=True)
 
-if os.path.dirname(os.getcwd()) == "/app":
-    d = "/app/air-quality-index-analysis-and-correlation-effect/Dashboard"
-else:
-    d=os.getcwd()
+# if os.path.dirname(os.getcwd()) == "/app":
+#     d = "/app/air-quality-index-analysis-and-correlation-effect/Dashboard"
+# else:
+#     d=os.getcwd()
 
-show_pdf(d+'/docs/portfolio.pdf')
+# show_pdf(d+'/docs/portfolio.pdf')
 
-pdf = os.getcwd()+'/docs/portfolio.pdf'
-st.markdown(f"""
-<iframe src="https://drive.google.com/uc?id=1UdXtGNRX1KC-qqIakEDLgxnm7V6heHa7" width="1300" height="650"></iframe>
-""", unsafe_allow_html=True)
+# pdf = os.getcwd()+'/docs/portfolio.pdf'
+# st.markdown(f"""
+# <iframe src="https://drive.google.com/uc?id=1UdXtGNRX1KC-qqIakEDLgxnm7V6heHa7" width="1300" height="650"></iframe>
+# """, unsafe_allow_html=True)
 
 pdf = os.getcwd()+'/docs/portfolio.pdf'
 st.markdown(f"""
