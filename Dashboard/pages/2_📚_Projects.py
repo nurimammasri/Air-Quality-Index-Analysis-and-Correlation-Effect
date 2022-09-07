5573eb4538fee7daeb1e14067350aad124c501c9
“nurimammasri” <54845293+nurimammasri@users.noreply.github.com>
2022-09-08 00:27:48 +0800

    Update 2_📚_Projects.py

--- Dashboard/pages/2_📚_Projects.py
+++ Dashboard/pages/2_📚_Projects.py
@@ -2,20 +2,29 @@ import streamlit as st
 import base64
 import os
 
+st.set_page_config(
+    layout="wide"
+)
+
 
 st.title("Projects")
 
-def show_pdf(file_path):
-    with open(file_path,"rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+# def show_pdf(file_path):
+#     with open(file_path,"rb") as f:
+#         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+#     pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+#     st.text(pdf_display)
+#     st.markdown(pdf_display, unsafe_allow_html=True)
 
-if os.path.dirname(os.getcwd()) == "/app":
-    d = "/app/air-quality-index-analysis-and-correlation-effect/Dashboard"
-else:
-    d=os.getcwd()
+# if os.path.dirname(os.getcwd()) == "/app":
+#     d = "/app/air-quality-index-analysis-and-correlation-effect/Dashboard"
+# else:
+#     d=os.getcwd()
 
-show_pdf(d+'/docs/portfolio.pdf')
+# show_pdf(d+'/docs/portfolio.pdf')
 
+pdf = os.getcwd()+'/docs/portfolio.pdf'
+st.markdown(f"""
+<embed src="https://drive.google.com/uc?id=1UdXtGNRX1KC-qqIakEDLgxnm7V6heHa7" width="1300" height="1000">
+""", unsafe_allow_html=True)
 
