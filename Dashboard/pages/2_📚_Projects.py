7d68c1b8b5174199227bcad61502c15723e390d9
“nurimammasri” <54845293+nurimammasri@users.noreply.github.com>
2022-09-07 12:38:00 +0800

    Update 2_📚_Projects.py

--- Dashboard/pages/2_📚_Projects.py
+++ Dashboard/pages/2_📚_Projects.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import base64
+import os
 
 
 st.title("Projects")
+
+def show_pdf(file_path):
+    with open(file_path,"rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+if os.path.dirname(os.getcwd()) == "/app":
+    d = "/app/air-quality-index-analysis-and-correlation-effect/Dashboard"
+else:
+    d=os.getcwd()
+
+show_pdf(d+'/docs/portfolio.pdf')
+
+
