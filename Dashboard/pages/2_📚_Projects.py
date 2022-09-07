0a7516d8a8b31fd3b47cbca1a40c9ce3fec27f38
“nurimammasri” <54845293+nurimammasri@users.noreply.github.com>
2022-09-08 01:33:26 +0800

    Update 2_📚_Projects.py

--- Dashboard/pages/2_📚_Projects.py
+++ Dashboard/pages/2_📚_Projects.py
@@ -13,7 +13,7 @@ def show_pdf(file_path):
     with open(file_path,"rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-    st.text(pdf_display)
+
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 if os.path.dirname(os.getcwd()) == "/app":
@@ -27,4 +27,3 @@ pdf = os.getcwd()+'/docs/portfolio.pdf'
 st.markdown(f"""
 <iframe src="https://drive.google.com/uc?id=1UdXtGNRX1KC-qqIakEDLgxnm7V6heHa7" width="1300" height="650"></iframe>
 """, unsafe_allow_html=True)
-
